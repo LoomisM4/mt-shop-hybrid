46126a36172e87f1ab5f0d5cd9e22c6777ec2fbd
LoomisM4 <mail@marcel-wettach.com>
2022-04-10 12:51:39 +0200

    fixed navigation

--- src/comp/Footer.js
+++ src/comp/Footer.js
@@ -1,25 +1,28 @@
 import React from "react";
 import '../style/Footer.css';
+import { useNavigate } from "react-router-dom";
 
 export default function Footer() {
+    let navigate = useNavigate();
+
     return(
         <footer>
-            <div className={"navLink"} onClick={() => window.location = "/"}>
+            <div className={"navLink"} onClick={() => navigate("/")}>
                 <div>
                     <img src="img/star.png" alt={"star"}/>
                 </div>
             </div>
-            <div className={"navLink"} onClick={() => window.location = "/categories"}>
+            <div className={"navLink"} onClick={() => navigate("/categories")}>
                 <div>
                     <img src="img/list.png" alt={"list"}/>
                 </div>
             </div>
-            <div className={"navLink"} onClick={() => window.location = "/cart"}>
+            <div className={"navLink"} onClick={() => navigate("/cart")}>
                 <div>
                     <img src="img/cart.png" alt={"cart"}/>
                 </div>
             </div>
-            <div className={"navLink"} onClick={() => window.location = "/map"}>
+            <div className={"navLink"} onClick={() => navigate("/map")}>
                 <div>
                     <img src="img/map.png" alt={"map"}/>
                 </div>
